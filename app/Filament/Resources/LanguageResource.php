438666d75ce01b435f3e9b3cb432beeaa6b7e139
Mahmoud El Sabahy <m.elsabahy@creativesolutions.com.sa>
2025-08-10 12:55:41 +0300

    allowed only super admin to manage platform data like currencies, languages and others

---
+++ app/Filament/Resources/LanguageResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use TomatoPHP\FilamentLocations\Resources\LanguageResource as BaseLanguageResource;
+
+class LanguageResource extends BaseLanguageResource
+{
+    public static function canViewAny(): bool
+    {
+        return auth()->user()?->isAdmin() ?? false;
+    }
+
+    public static function canCreate(): bool
+    {
+        return auth()->user()?->isAdmin() ?? false;
+    }
+
+    public static function canEdit($record): bool
+    {
+        return auth()->user()?->isAdmin() ?? false;
+    }
+
+    public static function canDelete($record): bool
+    {
+        return auth()->user()?->isAdmin() ?? false;
+    }
+
+    public static function canDeleteAny(): bool
+    {
+        return auth()->user()?->isAdmin() ?? false;
+    }
+}
